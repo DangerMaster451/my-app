00aedaf9ece58024c471fa71d6dd03ef510a6044
Andrew <andrewrpelton@outlook.com>
2026-01-17 01:35:16 -0800

    multiple album support

--- src/App.js
+++ src/App.js
@@ -9,38 +9,37 @@ const albums = [
     "artist": "The Japanese House",
     "cover": require('./assets/good at falling.jpg')
   },
+  {
+    "title": "In Rainbows",
+    "artist": "Radiohead",
+    "cover": require('./assets/in rainbows.jpg')
+  }
 ]
+
+const albumSize = '400px'
 
 function Album(index) {
   return (
     <div style={{
-      paddingTop: '10%',
-      height: '100vh',
-      backgroundColor: 'black'
+      margin: '10px'
     }}>
       <div style={{
-        display: 'flex',
-        alignItems: 'center',
         justifyContent: 'center',
       }}>
         <img src={albums[index].cover} style={{
-          width:"15%", 
-          height:"15%",
-          marginTop:"2%",
+          width:albumSize, 
+          height:albumSize,
           boxShadow: "rgba(255, 255, 255, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px",
           borderRadius: "2%",
         }}></img>
       </div>
       <div style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
         height:'auto',
         overflow: 'hidden',
       }}>
         <img src={albums[index].cover} style={{
-          width:"15%",
-          height:"15%",
+          width:albumSize,
+          height:albumSize,
           boxShadow: '1px 2px 9px black',
           borderRadius: "2%",
           transform: "scaleY(-1)",
@@ -54,23 +53,19 @@ function Album(index) {
       </div>
       
       <p style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
         fontFamily: 'Verdana',
         fontSize: '32px',
         fontWeight: 'bold',
-        marginTop: '-13%',
+        marginTop: '-350px',
+        textAlign: 'center',
         color: 'white',
       }}>{albums[index].title}</p>
 
       <p style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
         fontFamily: 'Verdana',
         fontWeight: 'bold',
         fontSize: '24px',
+        textAlign: 'center',
         color: 'white'
       }}>{albums[index].artist}</p>
     </div>
@@ -79,8 +74,18 @@ function Album(index) {
 
 function App() {
   return (
-    Album(0),
-    Album(1)
+    <div style={{
+      paddingTop: '10%',
+      height: '100vh',
+      backgroundColor: 'black',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+    }}>
+      {Album(0)},
+      {Album(1)},
+      {Album(2)}
+    </div>
   );
 
 }
